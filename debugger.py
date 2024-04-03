be0f835bf29e852023e5a04531fcad1cb4f78c7a
William Mills <wmpmills@gmail.com>
2024-04-03 09:36:56 -0500

    Create debugger.py

---
+++ debugger.py
@@ -0,0 +1,19 @@
+from lxml import etree
+from zeep import Plugin
+
+# This class lets you view the incoming and outgoing http headers and/or XML
+class DebugLogger( Plugin ):
+
+    def egress( self, envelope, http_headers, operation, binding_options ):
+
+        # Format the request body as pretty printed XML
+        xml = etree.tostring( envelope, pretty_print = True, encoding = 'unicode')
+
+        print( f'\nRequest\n-------\nHeaders:\n{http_headers}\n\nBody:\n{xml}' )
+
+    def ingress( self, envelope, http_headers, operation ):
+
+        # Format the response body as pretty printed XML
+        xml = etree.tostring( envelope, pretty_print = True, encoding = 'unicode')
+
+        print( f'\nResponse\n-------\nHeaders:\n{http_headers}\n\nBody:\n{xml}' )
